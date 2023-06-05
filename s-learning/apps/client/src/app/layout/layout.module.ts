59a93414ba44eaf7f85ff412499144d47bfd11da
KimLoiTrL <trlekimloi@gmail.com>
2023-06-05 16:44:53 +0700

    add home page

--- s-learning/apps/client/src/app/layout/layout.module.ts
+++ s-learning/apps/client/src/app/layout/layout.module.ts
@@ -4,17 +4,26 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NavigationComponent } from './navigation/navigation.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'welcome',
-    component: WelcomeComponent
+    component: WelcomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   }
-]
+];
 
 @NgModule({
-  declarations: [NavigationComponent, WelcomeComponent],
-  imports: [CommonModule, RouterModule, RouterModule.forChild(routes)],
-  exports: [NavigationComponent, WelcomeComponent],
+  declarations: [NavigationComponent, WelcomeComponent, HomeComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [NavigationComponent, WelcomeComponent, HomeComponent],
 })
 export class LayoutModule {}
