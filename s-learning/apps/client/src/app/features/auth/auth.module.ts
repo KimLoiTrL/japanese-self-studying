9b4d43cda2c0ff1d08630cdab1d946e9546329fa
KimLoiTrL <trlekimloi@gmail.com>
2023-06-25 18:34:41 +0700

    add registration page interface

--- s-learning/apps/client/src/app/features/auth/auth.module.ts
+++ s-learning/apps/client/src/app/features/auth/auth.module.ts
@@ -3,22 +3,21 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
-  }
-]
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-
-    RouterModule.forChild(routes)
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class AuthModule { }
+export class AuthModule {}
